93ce1dc156583a51a77592b0f67e68abeacef60b
Kailey Lowe <kl753916@ohio.edu>
2020-01-29 10:36:09 -0500

    added controller and did orders cs. 1/29/2020

---
+++ DAL/MIS4200Context.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using kl753916MIS4200.Models;
+
+namespace kl753916MIS4200.DAL
+{
+    public class MIS4200Context : DbContext
+    {
+        public MIS4200Context() : base("name=DefaultConnection")
+        {
+
+        }
+
+        public DbSet<customer> Customers { get; set; }
+        public DbSet<Orders> Orders { get; set; }
+
+    }
+}
