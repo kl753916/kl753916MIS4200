506245d0895f242f1e1329ac1d69289343e05fe5
Kailey Lowe <kl753916@ohio.edu>
2020-02-05 10:32:17 -0500

    Fixed the issue with the products page. Added in 1 of the codes that he had on the homepage. I added that to MIS4200Context

--- DAL/MIS4200Context.cs
+++ DAL/MIS4200Context.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
 using System.Linq;
 using System.Web;
 using kl753916MIS4200.Models;
@@ -11,13 +12,22 @@ namespace kl753916MIS4200.DAL
     {
         public MIS4200Context() : base("name=DefaultConnection")
         {
-
+            // add the SetInitializer statement here
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<MIS4200Context, kl753916MIS4200.Migrations.MISContext.Configuration>("DefaultConnection"));
         }
 
         public DbSet<customer> Customers { get; set; }
         public DbSet<Orders> Orders { get; set; }
         public DbSet<Products> Products { get; set; }
         public DbSet <OrderDetail> OrderDetails { get; set; }
+      
 
+        // add this method - it will be used later
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<OneToManyCascadeDeleteConvention>();
+            base.OnModelCreating(modelBuilder);
+        }
     }
+
 }
